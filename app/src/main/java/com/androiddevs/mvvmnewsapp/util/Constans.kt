3ddf0abc5bf4a3365c259819ef69cb64bb760522
Fannan Fauzan Fardhurohman <fannan@jkt1.ebdesk.com>
2021-11-20 22:51:13 +0700

    first commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constans.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constans {
+    companion object{
+        const val API_KEY = "8b506e2b3efc421d95a270f11b3a736e"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+    }
+}
